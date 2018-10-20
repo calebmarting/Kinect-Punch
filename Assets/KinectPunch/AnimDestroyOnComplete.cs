f8754dcb28e38a964eb8ffbd330f4759bf02d64a
Caleb Marting <cmarting99@gmail.com>
2018-10-20 16:30:45 -0500

    Scores

---
+++ Assets/KinectPunch/AnimDestroyOnComplete.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimDestroyOnComplete : MonoBehaviour {
+
+	private Animator anim;
+	public PunchScript handLeft;
+	public PunchScript handRight;
+	public float fuse;
+	public float fuseGracePeriod;
+
+	public GameObject ExplosionPrefab;
+
+	public ScoreManager scoreManager;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+		handLeft = GameObject.Find("Left Hand").GetComponent<PunchScript>();
+				handRight = GameObject.Find("Right Hand").GetComponent<PunchScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		fuse = fuse-Time.deltaTime;
+		if(fuse<=0){
+			if(fuse < 0){
+				fuseGracePeriod = fuseGracePeriod+fuse;//if it is less than 0 
+				fuse=0;
+			}else{
+				fuseGracePeriod -=Time.deltaTime;
+			}
+
+			if(handLeft.isPunching()||handRight.isPunching()){
+				if(fuseGracePeriod>0&&fuse==0){
+					GameObject clone = Instantiate(ExplosionPrefab);
+					if(handLeft.isPunching()){
+						clone.transform.position = handLeft.transform.position+Vector3.forward;
+					}
+					if(handRight.isPunching()){
+						clone.transform.position = handRight.transform.position+Vector3.forward;
+					}
+					scoreManager.calculateScore(fuseGracePeriod);
+					Destroy(gameObject);
+
+				}
+			}
+			
+		}
+
+
+
+		if(anim!=null){
+			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Finished")){
+				scoreManager.miss();
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	int totalScore = 0;
+	public GameObject[] ScoreTexts;
+	public int[] ScoreThresh;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void calculateScore(float time){
+		int score =(int)(100-Mathf.Abs(time-.15f)*100f/.15f);
+		totalScore+=score*10;
+
+		GameObject clone = null;
+		for(int i = 0; i<3 && clone == null; i++){
+			if(score>=ScoreThresh[i]){
+				clone = Instantiate(ScoreTexts[i]);
+				clone.transform.position = transform.position;
+			}
+		}
+
+		text.text = totalScore.ToString();
+		//Debug.Log(totalScore);
+	}
+
+	public void miss(){
+		totalScore-=500;
+
+		GameObject clone = Instantiate(ScoreTexts[3]);
+		clone.transform.position = transform.position;
+		text.text = totalScore.ToString();
+	}
+}
