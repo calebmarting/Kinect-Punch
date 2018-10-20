636fdd5fba80f893a26f4d69ccca5821e0257926
Caleb Marting <cmarting99@gmail.com>
2018-10-20 12:42:19 -0500

    Gonna fug everything with character controllers

---
+++ Assets/objectController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectController : MonoBehaviour {
+
+	public GameObject deleteOnHit;
+	public ParticleSystem playOnHit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!playOnHit.isPlaying && deleteOnHit == null)
+		{
+			Destroy(gameObject);
+		}
+	}
+	
+	void onHit (){
+		
+		Destroy(deleteOnHit);
+		playOnHit.Play();
+		
+	}
+	
+}
